dbd606054ad94b91cf321173e9d84cabbfc2053a
PolinaKokos <kosikpm@gmail.com>
2026-05-29 22:55:26 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -1,24 +1,25 @@
 // Copyright 2022 NNTU-CS
-#ifndef TRAIN_H
-#define TRAIN_H
+#ifndef INCLUDE_TRAIN_H_
+#define INCLUDE_TRAIN_H_
 class Train {
-private:
-    struct Car {
-        bool light;
-        Car* next;
-        Car* prev;
-        Car(bool lightState) : light(lightState), next(nullptr), prev(nullptr) {}
-    };
-    int countOp;
-    Car* first;
-    int length;
-public:
-    Train();
-    ~Train();
-    void addCar(bool light);
-    int getLength();
-    int getOpCount() const;
-    int getActualLength() const;
-    void clear();
+ private:
+  struct Car {
+    bool light;
+    Car* next;
+    Car* prev;
+    explicit Car(bool lightState)
+        : light(lightState), next(nullptr), prev(nullptr) {}
+  };
+  int countOp;
+  Car* first;
+  int length;
+ public:
+  Train();
+  ~Train();
+  void addCar(bool light);
+  int getLength();
+  int getOpCount() const;
+  int getActualLength() const;
+  void clear();
 };
-#endif
+#endif  // INCLUDE_TRAIN_H_
