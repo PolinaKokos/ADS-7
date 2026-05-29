0d47fdda828a1c1c5230d234b8ca7fcbf9f70af0
PolinaKokos <kosikpm@gmail.com>
2026-05-29 21:30:07 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -1,7 +1,6 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TRAIN_H_
 #define INCLUDE_TRAIN_H_
-
 class Train {
  private:
   struct Car {
@@ -9,20 +8,16 @@ class Train {
     Car* next;
     Car* prev;
   };
-
   int countOp;
   Car* first;
   int length;
-
  public:
   Train();
   ~Train();
-
   void addCar(bool light);
   int getLength();
   int getOpCount() const;
   int getActualLength() const;
   void clear();
 };
-
 #endif  // INCLUDE_TRAIN_H_
